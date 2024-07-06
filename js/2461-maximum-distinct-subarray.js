b5d7ae3f4775503e62cea3a70d3a765b3c47294f
Jeri Sommers <noreply@example.com>
2024-07-06 11:28:49 -0700

    fix bug in Set logic

--- js/2461-maximum-distinct-subarray.js
+++ js/2461-maximum-distinct-subarray.js
@@ -1,42 +1,37 @@
 /**
  * https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/description/
  * O(n) runtime, uses O(1) space
- * could be optimized to use a little less space and lookups by using array instead of set and
- *   then pulling lastNum from array.
  * added some test cases to validate edge cases and other sizes:
  * [1] 1 // initially tried [1] 3 but base callouts included k not bigger than array size
  * [1,5,6,4,6] 4
- * @param {number[]} nums
+ * [1,5,6,4,6,5,9] 4
+ * @param {number[]} nums 
  * @param {number} k size of subarray
  * @return {number}
  */
 var maximumSubarraySum = function(nums, k) {
     if (nums.length < k) return 0;
 
-    let i=0, max=0, windowSum=0;
-    let window = new Set();
-    let lastNum;
+    let i=0, max=0, windowSum=0, windowArr=[];
+    let window;
 
     // first window
     for (; i < k; i++) {
         windowSum += nums[i];
-        window.add(nums[i]);
+        windowArr.push(nums[i]);
     }
-    if (window.size === k) {
-        max = windowSum;
-        lastNum = nums[0];
-    }
+    window = new Set(windowArr);
+    if (window.size === k) max = windowSum;
 
     // remaining windows
     for (; i < nums.length; i++) {
         // cleanup last window
-        window.delete(lastNum);
-        windowSum -= lastNum;
+        windowSum -= windowArr.shift();
 
         // add/check new window
         windowSum += nums[i];
-        window.add(nums[i]);
-        lastNum = nums[i-k+1]; // array index shenanigans
+        windowArr.push(nums[i]);
+        window = new Set(windowArr);
 
         if (window.size === k &&
             windowSum > max) {
