1f0383a44d4a83c48fc162054229bedd891bd20a
🐍 Jeri Sommers <10818509+sojeri@users.noreply.github.com>
2024-07-15 11:32:33 -0700

    swap fn order so initial solution is first in code

--- js/2958-longest-subarray-max-dupes.js
+++ js/2958-longest-subarray-max-dupes.js
@@ -9,6 +9,40 @@ var maxSubarrayLength = function(nums, k) {
 };
 
 /**
+ * initial brute force solution
+ * runtime O(n^2) because for every n, might have to check every other n
+ * space is O(2n) since I shove everything into a map each outer loop
+ * @param {*} nums 
+ * @param {*} k 
+ * @returns 
+ */
+function maxNaive(nums, k) {
+    if (k==1 && nums.length == 1) return 1;
+    let max = -1;
+    for (let i=0; i< nums.length; i++) {
+        const dict = { [nums[i]]: 1 }
+        for (let j=i+1; j < nums.length; j++) {
+            const current = nums[j];
+            if (dict[current] == k)  {
+                max = Math.max(j - i, max);
+                break;
+            }
+
+            if (dict[current]) {
+                dict[current] += 1;
+            } else {
+                dict[current] = 1;
+            }
+
+            if (j + 1 == nums.length) {
+                max = Math.max(j - i + 1, max);
+            }
+        }
+    }
+    return max;
+}
+
+/**
  * second solution using a more sliding window-y approach to solving
  * runtime O(n) since only checking each element approx once
  * space is still O(2n) because I throw everything in a map
@@ -51,40 +85,6 @@ function maxLessNaive(nums, k) {
         }
         
         j++;
-    }
-    return max;
-}
-
-/**
- * initial brute force solution
- * runtime O(n^2) because for every n, might have to check every other n
- * space is O(2n) since I shove everything into a map each outer loop
- * @param {*} nums 
- * @param {*} k 
- * @returns 
- */
-function maxNaive(nums, k) {
-    if (k==1 && nums.length == 1) return 1;
-    let max = -1;
-    for (let i=0; i< nums.length; i++) {
-        const dict = { [nums[i]]: 1 }
-        for (let j=i+1; j < nums.length; j++) {
-            const current = nums[j];
-            if (dict[current] == k)  {
-                max = Math.max(j - i, max);
-                break;
-            }
-
-            if (dict[current]) {
-                dict[current] += 1;
-            } else {
-                dict[current] = 1;
-            }
-
-            if (j + 1 == nums.length) {
-                max = Math.max(j - i + 1, max);
-            }
-        }
     }
     return max;
 }
